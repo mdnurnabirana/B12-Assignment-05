bc1788c57e9708ac7b1125218a6689b0b4e39b60
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-08-28 00:12:22 +0600

    Add heart icon (wishlist) increment functionality

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.getElementById("header-bar").addEventListener("click", function() {
         mobileMenu.classList.replace("flex", "hidden");
     }
 });
+
+// Heart Icon Functionalities
+let buttons = document.getElementsByClassName("heart-icon-click");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function (e) {
+    let heartCountElem1 = document.getElementById("heart-count1");
+    let heartCnt1 = parseInt(heartCountElem1.innerText, 10);
+    heartCnt1++;
+    heartCountElem1.innerText = heartCnt1;
+
+    let heartCountElem2 = document.getElementById("heart-count2");
+    let heartCnt2 = parseInt(heartCountElem2.innerText, 10);
+    heartCnt2++;
+    heartCountElem2.innerText = heartCnt2;
+  });
+}
